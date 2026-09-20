530814a2a67d99467621d25600f6eb4ac4eb3019
shanregs <shanregs@gmail.com>
2026-09-20 10:41:06 +0530

    chore: initialize project repository

---
+++ src/test/java/com/shan/kafka/kafkaprodcons/KafkaProdConsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shan.kafka.kafkaprodcons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProdConsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
